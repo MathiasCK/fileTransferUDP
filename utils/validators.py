188a0011afbb2949e73e17df1c686534e40c3769
mathiasck <mathias.ck2000@gmail.com>
2023-04-27 13:37:43 +0200

    Update validators.py: add comments

--- utils/validators.py
+++ utils/validators.py
@@ -17,16 +17,23 @@ def isValidPort(port):
    if not int(port) >= 1024 and int(port) <= 65535:
       responses.syntaxError("Please provide a valid port (range 1024 -> 65535)")
 
+# Checks if file provided by client exists in tree
+# @file_path -> path of file
+# Returns file_path if valid 
 def isValidFile(file_path):
    if Path(f"./{file_path}").is_file() is False:
       responses.syntaxError('Image path is not valid')
    return file_path
 
+# Checks if trigger provided by client or server is valid
+# @arg -> -t flag value
 def isValidTrigger(arg):
    isValid = re.match(r"^(?:skipack|loss)$", arg, re.IGNORECASE)
    if str(isValid) == "None":
        responses.syntaxError("Please provide a valid trigger format (skipack/loss)")
 
+# Checks if reliability provided by client or server is valid
+# @arg -> -r flag value
 def isValidReliability(arg):
    isValid = re.match(r"^(?:saw|gbn|sr)$", arg, re.IGNORECASE)
    if str(isValid) == "None":
